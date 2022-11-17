2a3a031ba5450811497a0b2d9d197cb9bf3ac14d
Julius Kamara <juliuskamara79@gmail.com>
2022-11-16 17:37:29 -0800

    Added new files

--- shell.h
+++ shell.h
@@ -1,160 +1,94 @@
+/* 
+ *    Programmed By: Mohammed Isam [mohammed_isam1984@yahoo.com]
+ *    Copyright 2020 (c)
+ * 
+ *    file: shell.h
+ *    This file is part of the "Let's Build a Linux Shell" tutorial.
+ *
+ *    This tutorial is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This tutorial is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this tutorial.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include <stddef.h>     /* size_t */
+#include <glob.h>
+#include "source.h"
 
-#define BUFFER_SIZE 1024
-#define ENV_SEPARATOR "="
-#define ESCAPE_SEPARATOR "#"
-#define PATH_SEPARATOR ":"
-#define COMMAND_SEPARATOR ";\n"
-#define SEPARATORS " \n"
-#define PROMPT "$ "
+void print_prompt1(void);
+void print_prompt2(void);
+char *read_cmd(void);
+int  parse_and_execute(struct source_s *src);
 
-extern char **environ;
+void initsh(void);
 
-/**
- * struct environment_s - environment variable
- *
- * @name: environment name
- * @value: environment value
- * @next: points to the next node
- */
-typedef struct environment_s
+/* shell builtin utilities */
+int dump(int argc, char **argv);
+
+/* struct for builtin utilities */
+struct builtin_s
 {
-	char *name;   /* ex: PATH */
-	char *value;  /* ex: /bin:/usr/bin */
-	char *global; /* PATH=/bin:/usr/bin */
-	struct environment_s *next;
-} environment_t;
+    char *name;    /* utility name */
+    int (*func)(int argc, char **argv); /* function to call to execute the utility */
+};
 
-/**
- * struct appData_s - data variable
- *
- * @arguments: argument's array
- * @buffer: buffer
- * @command: command name
- */
-typedef struct appData_s
+/* the list of builtin utilities */
+extern struct builtin_s builtins[];
+
+/* and their count */
+extern int builtins_count;
+
+/* struct to represent the words resulting from word expansion */
+struct word_s
 {
-	char **arguments;
-	char *buffer;
-	char *commandName;
-	char **commandList;
-	char **history;
-	char *programName;
-	environment_t *env;
-} appData_t;
+    char  *data;
+    int    len;
+    struct word_s *next;
+};
 
-/**
- * struct errorMessage_s - An structure for each error message
- *
- * @ecode: error code
- * @msg: pointer to error message
- * @size: error message length.
- */
-typedef struct errorMessage_s
-{
-	int code;
-	char *msg;
-} errorMessage_t;
+/* word expansion functions */
+struct  word_s *make_word(char *word);
+void    free_all_words(struct word_s *first);
 
-/**
- * struct customCommand_s - struct conversion to function
- *
- * @command: flag string
- * @func: pointer to func
- */
-typedef struct customCommand_s
-{
-	char *commandName;
-	void (*func)(appData_t *);
-} customCommand_t;
+size_t  find_closing_quote(char *data);
+size_t  find_closing_brace(char *data);
+void    delete_char_at(char *str, size_t index);
+char   *substitute_str(char *s1, char *s2, size_t start, size_t end);
+char   *wordlist_to_str(struct word_s *word);
 
-environment_t *_addEnvNodeEnd(
-	environment_t **prmHeadNode,
-	char *prmGlobal
-);
-void _addWord(char *prmWord, int *prmIndex, char **prmArray);
-int _atoi(char *prmString);
-void *_calloc(unsigned int prmNumber, unsigned int prmSize);
-void _cdHelp(void);
-void _changeDirectory(appData_t *prmData);
-void _changeToAnyDirectory(appData_t *prmData, char *prmCurrentDirectory);
-void _changeToHomeDirectory(appData_t *prmData, char *prmCurrentDirectory);
-void _changeToPreviousDirectory(appData_t *prmData, char *prmCurrentDirectory);
-int _checkEndCharacter(char *prmString);
-int _checkEscapeSeparators(char prmChar, char *prmEscapeSeparators);
-int _checkSeparators(char prmChar, char *prmSeparators);
-char *_cleanString(char *prmString);
-environment_t *_createEnvNode(char *prmGlobal);
-void _ctrlC(int prmSignal);
-void _defaultHelp(char *prmCommand);
-int _deleteEnvNode(environment_t *prmHead, char *prmName);
-void _prompt(void);
-void _env(appData_t *prmData);
-void _envHelp(void);
-void _errorHandler(appData_t *prmData, int messageCode);
-void _execCommand(appData_t *prmData);
-void _exitStatus(appData_t *prmData);
-void _exitHelp(void);
-void _freeAppData(appData_t *prmData);
-void _freeCharDoublePointer(char **prmPtr);
-void _freeEnvList(environment_t *prmHeadNode);
-char *_generateAbsolutePath(char *prmPath, char *prmCommandName);
-char *_generateEnvGlobal(char *prmName, char *prmValue);
-void (*_getCustomFunction(char *prmCommand))(appData_t *);
-environment_t *_getenv(environment_t *prmEnviron, char *prmName);
-char *_getenvname(char *prmVariable);
-char *_getenvvalue(char *prmVariable);
-int _getEnvIndex(environment_t *prmHead, char *prmName);
-environment_t *_getEnvNodeAtIndex(
-	environment_t *prmHead,
-	unsigned int prmIndex
-);
-environment_t *_getLastEnvNode(environment_t *prmHeadNode);
-void _getline(appData_t *prmData);
-char *_getword(char *prmGlobal, int prmOffset, int prmSize);
-void _help(appData_t *prmData);
-void _helpHelp(void);
-int _inArray(char prmChar, char *prmArray);
-appData_t *_initData(char **prmArgv);
-void _initEnvData(appData_t *prmData);
-int _isdigit(char prmChar);
-int _isNumber(char *s);
-char *_itoa(int prmNumber);
-int _listEnvLen(environment_t *prmHead);
-char *_memcpy(char *prmDest, char *prmSrc, size_t prmLimit);
-char *_memset(char *prmString, char prmCharacter, unsigned int prmLimit);
-int _nbrLen(int prmNumber);
-char **_parsingPathEnvironment(appData_t *prmData);
-void _printenv(environment_t *prmEnviron);
-int _putchar(char prmChar);
-int _puts(char *prmStr);
-void *_realloc(void *prmPtr, unsigned int prmOldSize, unsigned int prmNewSize);
-void _setenv(environment_t *prmEnviron, char *prmName, char *prmValue, int prmOverwrite);
-void _setenvHelp(void);
-void _setEnvironment(appData_t *prmData);
-char *_strcat(char *prmDest, char *prmSrc);
-int _strcmp(char *prmString1, char *prmString2);
-char *_strcpy(char *prmDest, char *prmSrc);
-char *_strconcat(char *prmString1, char *prmString2);
-char *_strncpy(char *prmDest, char *prmSrc, int prmLimit);
-unsigned int _strcspn(char *prmString, char *prmDeny);
-char *_strdup(char *prmString);
-int _strlen(char *prmStr);
-char *_strstr(char *prmHaystack, char *prmNeedle, int prmBegin);
-char **_strtow(char *prmString, char *prmSeparators, char *prmEscapeSeparators);
-void _unsetenv(appData_t *prmData, char *prmName);
-void _unsetenvHelp(void);
-void _unsetEnvironment(appData_t *prmData);
-char *_which(appData_t *prmData);
-int _wordNumber(char *prmString, char *prmSeparators);
+struct  word_s *word_expand(char *orig_word);
+char   *word_expand_to_str(char *word);
+char   *tilde_expand(char *s);
+char   *command_substitute(char *__cmd);
+char   *var_expand(char *__var_name);
+char   *pos_params_expand(char *tmp, int in_double_quotes);
+struct  word_s *pathnames_expand(struct word_s *words);
+struct  word_s *field_split(char *str);
+void    remove_quotes(struct word_s *wordlist);
+
+char   *arithm_expand(char *__expr);
+
+/* some string manipulation functions */
+char   *strchr_any(char *string, char *chars);
+char   *quote_val(char *val, int add_quotes);
+int     check_buffer_bounds(int *count, int *len, char ***buf);
+void    free_buffer(int len, char **buf);
+
+/* pattern matching functions */
+int     has_glob_chars(char *p, size_t len);
+int     match_prefix(char *pattern, char *str, int longest);
+int     match_suffix(char *pattern, char *str, int longest);
+char  **get_filename_matches(char *pattern, glob_t *matches);
 
 #endif
